a3c1be2ce7bffe10c50bac7c99ed79f6c18a2d07
jimmybutler-2015 <image345678@outlook.com>
2019-10-06 14:22:25 +0800

    minor changes

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
-html = urlopen("http://www.pythonscraping.com/pages/page1.html")
-bsObj = BeautifulSoup(html.read())
-print(bsObj.h1)
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read())
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+title = getTitle("http://www.pythonscraping.com/pages/warandpeace.html")
+if title == None:
+    print("Title could not be found")
+else:
+    print(title)
+
